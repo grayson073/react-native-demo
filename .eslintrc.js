62014106bea1cbf64fa4251b4769cc29f8a688c5
Kevin Grayson <k.grayson.ms@gmail.com>
2019-02-05 21:08:51 -0800

    Setup for react-native demo

---
+++ .eslintrc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  'extends': [
+    'airbnb',
+    'plugin:flowtype/recommended'
+  ],
+  'plugins': [
+    'flowtype'
+  ],
+  'parser': 'babel-eslint',
+  'env': {
+    'jest': true,
+  },
+  'rules': {
+    'no-use-before-define': 'off',
+    'react/jsx-filename-extension': 'off',
+    'react/prop-types': 'off',
+    'comma-dangle': 'off'
+  },
+  'globals': {
+    "fetch": false
+  }
+}
